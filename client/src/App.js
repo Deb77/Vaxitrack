ec2c0ce9b1578644c392b3372fcc8b81f548a4b0
Deborah <deb.mendes.9@gmail.com>
2020-11-10 08:58:55 +0530

    Redux store and react routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Parent from './pages/Parent';
+import Doctor from './pages/Doctor';
+import Child from './pages/Child';
+import Admin from './pages/Admin';
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Provider store={store}>
+      <Router>
+        <h1>Hell</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/parent" component={Parent} />
+          <Route exact path="/parent/child" component={Child} />
+          <Route exact path="/admin" component={Admin} />
+          <Route exact path="/admin" component={Doctor} />
+          <Route exact path="/admin/child" component={Child} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
