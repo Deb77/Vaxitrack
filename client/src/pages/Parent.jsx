cc0b1edd69a34905812a17c7e13a092a58c0f935
Deborah <deb.mendes.9@gmail.com>
2020-12-12 14:09:15 +0530

    Initial setup

--- client/src/pages/Parent.jsx
+++ client/src/pages/Parent.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {
+    FormContainer,
+    TextField
+} from '../components/FormElements';
+
+const ParentForm = () => {
+
+}
 
 const Parent = () => {
+    const [name, setName] = useState("");
+    const [DOB, setDOB] = useState(new Date());
+
     return (
-        <div>
-            Parent Page
-        </div>
+        <FormContainer>
+            <TextField
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <DatePicker
+                selected={DOB}
+                onChange={date => setDOB(date)}
+            />
+        </FormContainer>
     )
 }
 
---
+++ client/src/services/childService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { baseUrl } from '../config/config';
+
+export const childService = {
+    post,
+    put,
+    del
+}
+
+function post(apiEndpoint, payload) {
+    return axios.post(baseUrl + 'child/'+apiEndpoint,payload)
+}
+
+function put(apiEndpoint, payload) {
+    return axios.put(baseUrl + 'child/'+ apiEndpoint,payload)
+}
+
+function del (apiEndpoint, payload){
+    return axios.delete(baseUrl + 'child/' + apiEndpoint, payload);
+}
